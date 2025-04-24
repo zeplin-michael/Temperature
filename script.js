bcb1584cd6405e84678b13e8bc3a23bef5a55a1b
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-04-24 17:23:32 -0400

    Updated Javascript with Prompt

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let Fahrenheit = Number(prompt("Please enter a temperature in Fahrenheit?"));
+console.log(Fahrenheit + " Fahrenheit");
 
 function convertToCelsius(Fahrenheit) {
   return (5 / 9) * (Fahrenheit - 32);
 }
-
-console.log(convertToCelsius(Fahrenheit));
+console.log(convertToCelsius(Fahrenheit) + " Celsius");
 
 function describeTemperature(Fahrenheit) {
-  if (Fahrenheit < 0) {
+  if (convertToCelsius(Fahrenheit) < 0) {
     console.log("Very Cold");
-  } else if (Fahrenheit < 20) {
+  } else if (convertToCelsius(Fahrenheit) < 20) {
     console.log("Cold");
-  } else if (Fahrenheit < 30) {
+  } else if (convertToCelsius(Fahrenheit) < 30) {
     console.log("Warm");
-  } else if (Fahrenheit < 40) {
+  } else if (convertToCelsius(Fahrenheit) < 40) {
     console.log("Hot");
-  } else if (40 <= Fahrenheit) {
+  } else if (40 <= convertToCelsius(Fahrenheit)) {
     console.log("Very Hot");
   } else {
     console.log("No number detected!");
   }
 }
 
-console.log(describeTemperature(Fahrenheit));
+describeTemperature(Fahrenheit);
